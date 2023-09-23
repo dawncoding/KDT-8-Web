2c1b4c5bccfce1504c8d1fcc38ac871e2eb741ce
dawncoding <sunnuri99@gmail.com>
2023-09-23 10:38:19 +0900

    upload react study code folders

---
+++ react_basic/src/App.js
@@ -0,0 +1,13 @@
+import LifeCycleClass from './08Lifecycle/LifeCycleClass';
+import LifeCycleFunc from './08Lifecycle/LifeCycleFunc';
+
+function App() {
+    return (
+        <>
+            {/* <LifeCycleClass /> */}
+            <LifeCycleFunc />
+        </>
+    );
+}
+
+export default App;
